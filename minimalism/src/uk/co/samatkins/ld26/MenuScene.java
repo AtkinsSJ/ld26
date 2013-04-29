0dfcea9e5fd880c6b26f6e96dcd444db5d71bf40
Sam Atkins <github@samatkins.co.uk>
2013-04-29 19:48:49 +0100

    Moved skin loading to main class.

---
+++ minimalism/src/uk/co/samatkins/ld26/MenuScene.java
@@ -0,0 +1,23 @@
+package uk.co.samatkins.ld26;
+
+import com.badlogic.gdx.graphics.Color;
+
+public class MenuScene extends Scene {
+
+	public MenuScene(LD26 game) {
+		super(game, Color.WHITE);
+	}
+	
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		super.show();
+	}
+
+	@Override
+	public void update(float delta) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
