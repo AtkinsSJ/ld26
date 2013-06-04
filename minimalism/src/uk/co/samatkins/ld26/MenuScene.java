7078bc9049f1b0961a77752c2130771f1b49cd63
Sam Atkins <github@samatkins.co.uk>
2013-06-04 17:25:20 +0100

    No idea, but for some reason there are uncommitted changes.

--- minimalism/src/uk/co/samatkins/ld26/MenuScene.java
+++ minimalism/src/uk/co/samatkins/ld26/MenuScene.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.utils.Align;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 public class MenuScene extends Scene {
@@ -28,6 +29,13 @@ public class MenuScene extends Scene {
 		
 		this.table.add("DropPix").expand().row();
 		this.table.add("A Ludum Dare 26 Jam entry by Sam Atkins").row();
+		
+		Label l = new Label("Click the buttons around the edge to fire pixels into the grid. "
+				+ "They'll stop when they hit an existing pixel. "
+				+ "Keep adding pixels to match the target image. Be careful not to block yourself!", skin);
+		l.setWrap(true);
+		l.setAlignment(Align.center);
+		this.table.add(l).expand().fill().row();
 		
 		TextButton playButton = new TextButton("Play", skin);
 		playButton.addListener(new ClickListener() {
--- minimalism/src/uk/co/samatkins/ld26/PlayScene.java
+++ minimalism/src/uk/co/samatkins/ld26/PlayScene.java
@@ -5,12 +5,15 @@ import uk.co.samatkins.ld26.ImageRenderer.BlockType;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.scenes.scene2d.EventListener;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.Align;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-import com.badlogic.gdx.utils.Array;
 
 public class PlayScene extends Scene {
 	
